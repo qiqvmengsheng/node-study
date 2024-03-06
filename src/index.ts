009f164039e419fec623dad2ff36766bb9901146
luoziqi <qiqvmengchu@outlook.com>
2024-03-06 23:44:18 +0800

    feat: 添加express

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import './study/util工具'
 
+import express, { Express } from 'express'
+const app: Express = express()
+app.get('/api/jsonp', (req, res) => {
+  const { callback } = req.query
+  console.log(callback)
+
+  res.send(`${callback}('Hello World!！！！！！！！！！')`)
+})
+app.listen(3000, () => {
+  console.log('http://localhost:3000')
+})
+
 const main = () => {}
 
 main()
